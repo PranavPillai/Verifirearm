2d03bdbc5a863f8cb7e1ce2bd52ce69805629040
Pranav Pillai <pranavp802@gmail.com>
2022-06-03 14:09:54 -0700

    vue starter kit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <router-view />
+</template>
+<script>
+import { defineComponent } from "vue";
+import { hasSession } from "@verida/account-web-vault";
+
+const { VUE_APP_CONTEXT_NAME } = process.env;
+console.log(VUE_APP_CONTEXT_NAME);
+
+export default defineComponent({
+  name: "App",
+  props: {},
+  data() {
+    return {
+      isLoggedIn: hasSession(VUE_APP_CONTEXT_NAME ? VUE_APP_CONTEXT_NAME : ''),
+    };
+  },
+
+  methods: {
+    setLogin() {
+      this.isLoggedIn = hasSession(VUE_APP_CONTEXT_NAME ? VUE_APP_CONTEXT_NAME : '');
+    },
+  },
+});
+</script>
