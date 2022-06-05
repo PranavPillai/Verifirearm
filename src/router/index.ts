e9f3ac20d3e72ba329078274cb2ffe5dd76756ad
Nicholas Parkes <nickparkes7@gmail.com>
2022-06-05 08:15:38 -0700

    old stuff commit

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,10 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import Credential from "../views/Home.vue";
 import SSOLogin from "../views/SSOLogin.vue";
-<<<<<<< HEAD
 import DMV from "../views/DMV.vue";
 import Health from "../views/HealthCredentialer.vue"
 
 const { VUE_APP_CONTEXT_NAME } = process.env;
-=======
-import { routeGuard } from "../helpers/RouteGaurd";
->>>>>>> 7621129185d5da92bfdaf99a44a3b0652804676d
 
 const routes: Array<RouteRecordRaw> = [
   {
