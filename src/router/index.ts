c83ea99e3e4e323c791152674072c07d84c56121
Nicholas Parkes <nickparkes7@gmail.com>
2022-06-05 08:29:58 -0700

    added health and crime pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import Credential from "../views/Home.vue";
 import SSOLogin from "../views/SSOLogin.vue";
+import Health from "../views/HealthCredentialer.vue";
+import Crime from "../views/CrimeCredentialer.vue";
 import { routeGuard } from "../helpers/RouteGaurd";
 
 const routes: Array<RouteRecordRaw> = [
@@ -16,6 +18,16 @@ const routes: Array<RouteRecordRaw> = [
     path: "/connect",
     name: "Connect",
     component: SSOLogin,
+  },
+  {
+    path: "/health",
+    name: "Health",
+    component: Health,
+  },
+  {
+    path: "/connect",
+    name: "Connect",
+    component: Crime,
   },
 ];
 
